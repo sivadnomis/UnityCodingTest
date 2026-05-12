e8893d3f729058c8f1f7cf4016545f935997407a
Simon Davis <32305291+sivadnomis@users.noreply.github.com>
2026-05-12 15:28:35 +0100

    Created reward assets

--- Assets/Scripts/Rewards/BundleRewardItem.cs
+++ Assets/Scripts/Rewards/BundleRewardItem.cs
@@ -4,6 +4,5 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "NewBundleReward", menuName = "Rewards/Bundle Reward")]
 public class BundleRewardItem : RewardItem
 {
-    [Range(1, 4)]
     public List<SingleRewardItem> items = new List<SingleRewardItem>();
 }
